4107096d5877b8b40e7c52c091c8193df4dec821
Jojo Masala <jojomasala@protonmail.com>
2018-08-18 02:51:44 +0100

    Addded a splash screen/initial screen with the app logo. Looks nice! Achieved without having to create a new activity xml or js file so feels elegant.
    The splash screen should only appear for as long as the application is loading; it should not delay the user for a specified time.

--- app/src/main/java/com/example/android/crimen/MainActivity.java
+++ app/src/main/java/com/example/android/crimen/MainActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Make sure this is before calling super.onCreate
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
