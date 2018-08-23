dd115be10a6ec968bae81ae9f7d03845be7eb6f7
Jojo Masala <jojomasala@protonmail.com>
2018-08-23 20:27:00 +0100

    Imported module for persistent search bar and updated the dependecies also. Styled and added persistent search bar to the tree.

--- app/src/main/java/com/example/android/crimen/MainActivity.java
+++ app/src/main/java/com/example/android/crimen/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.android.crimen;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.arlib.floatingsearchview.FloatingSearchView;
+
 public class MainActivity extends AppCompatActivity {
 
+    // The floating search bar that remains at the top of the application
+    private FloatingSearchView mFloatingSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFloatingSearchView = (FloatingSearchView) findViewById(R.id.floating_search_view);
+
+        mFloatingSearchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
+            @Override
+            public void onSearchTextChanged(String oldQuery, final String newQuery) {
+
+                //get suggestions based on newQuery
+
+                //pass them on to the search view
+                //mFloatingSearchView.swapSuggestions(newSuggestions);
+            }
+        });
     }
 }
