7802f6c36b94ca513d790b4c6bc78abcad68385d
Jojo Masala <jojomasala@protonmail.com>
2018-08-25 23:03:25 +0100

    Place Autocomplete API added from Google Places API so places are auto suggested when the user searches for one. Updated dependencies.

--- app/src/main/java/com/example/android/crimen/MainActivity.java
+++ app/src/main/java/com/example/android/crimen/MainActivity.java
@@ -6,20 +6,40 @@ import android.os.Bundle;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.PlaceDetectionClient;
 import android.support.v4.app.FragmentActivity;
+import android.widget.RelativeLayout;
+
 import com.arlib.floatingsearchview.FloatingSearchView;
 import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 
 public class MainActivity extends AppCompatActivity {
+
+    //
+    private static final LatLngBounds BOUNDS_LONDON = new LatLngBounds(new LatLng(57.149651, -2.099075), new LatLng(51.621441, -3.943646));
 
     // The floating search bar that remains at the top of the application
     private FloatingSearchView mFloatingSearchView;
 
     // The Google Place API request code
     private static final int  PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+
+    // The drawer that opens when the search bar is pressed and displays options
+    private RelativeLayout autocompleteRelativeLayout;
+
+    LatLngBounds.Builder latlngBuilder = new LatLngBounds.Builder();
+
+    private GoogleApiClient mGoogleApiClient;
+
+
+    // The adapter for the autocompleted places
+    private PlaceAutoCompleteInterfacemAdapter mAutocompletAdapter = new PlaceAutoCompleteInterface(this, R.id.floating_search_view, mGoogleApiClient, BOUNDS_LONDON, TYPE_FILTER_NONE );
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +48,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Find a reference to the RelativeLayout prediction drawer
+        autocompleteRelativeLayout = (RelativeLayout) findViewById(R.id.predictedRow);
+
         // Handles what happens with the floating searchview
         mFloatingSearchView = findViewById(R.id.floating_search_view);
 
         mFloatingSearchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
             @Override
             public void onSearchTextChanged(String oldQuery, final String newQuery) {
-
-                try {
-                    Intent intent =
-                            new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
-                                    .build(MainActivity.this);
-                    startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
-                } catch (GooglePlayServicesRepairableException e) {
-                    // TODO: Handle the error.
-                } catch (GooglePlayServicesNotAvailableException e) {
-                    // TODO: Handle the error.
-                }
 
                 //get suggestions based on newQuery
 
