23795daa35a95e32390d9c5df9be58ba089bf3da
Jojo Masala <jojomasala@protonmail.com>
2018-08-25 16:42:26 +0100

    Place Autocomplete API added from Google Places API so places are auto suggested when the user searches for one. Updated dependencies.

--- app/src/main/java/com/example/android/crimen/MainActivity.java
+++ app/src/main/java/com/example/android/crimen/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.android.crimen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import android.support.v4.app.FragmentActivity;
 import com.arlib.floatingsearchview.FloatingSearchView;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 
 public class MainActivity extends AppCompatActivity {
 
     // The floating search bar that remains at the top of the application
     private FloatingSearchView mFloatingSearchView;
+
+    // The Google Place API request code
+    private static final int  PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +28,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFloatingSearchView = (FloatingSearchView) findViewById(R.id.floating_search_view);
+        // Handles what happens with the floating searchview
+        mFloatingSearchView = findViewById(R.id.floating_search_view);
 
         mFloatingSearchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
             @Override
             public void onSearchTextChanged(String oldQuery, final String newQuery) {
+
+                try {
+                    Intent intent =
+                            new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                                    .build(MainActivity.this);
+                    startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+                } catch (GooglePlayServicesRepairableException e) {
+                    // TODO: Handle the error.
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    // TODO: Handle the error.
+                }
 
                 //get suggestions based on newQuery
 
